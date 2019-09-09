0747dbd0818c06eb45b01e066bb3a9d99ea1fc91
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2019-09-09 15:57:00 +0200

    Initial commit

---
+++ Install-FireFox.ps1
@@ -0,0 +1,5 @@
+$SourceURL = "https://download.mozilla.org/?product=firefox-msi-latest-ssl&os=win64&lang=en-US";
+$Installer = $env:TEMP + "\firefox.exe"; 
+Invoke-WebRequest $SourceURL -OutFile $Installer;
+Start-Process -FilePath $Installer -Args "/s" -Verb RunAs -Wait; 
+Remove-Item $Installer;
