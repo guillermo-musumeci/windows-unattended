2153f1bcc10bb9e0b972de3934c90c6b6bc239ac
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2019-09-09 15:16:09 +0200

    Initial commit

---
+++ Set-IP.ps1
@@ -0,0 +1,18 @@
+# Variables
+$IPAddress = "192.168.56.135"
+$IPSubnetPrefix = "24"
+$IPGateway = "192.168.56.1"
+
+# Get Active Network Interface Name
+$InterfaceName = Get-NetAdapter -physical | Where-Object status -eq 'up' | Select-Object Name
+
+# Reset Network Settings
+Remove-NetRoute -DestinationPrefix "0.0.0.0/0" -Confirm:$false
+Get-NetIPAddress -InterfaceAlias $InterfaceName.Name | Remove-NetIpAddress  -Confirm:$false
+
+# Set Network Settings
+New-NetIpAddress `
+-InterfaceAlias $InterfaceName.Name `
+-IpAddress $IPAddress `
+-PrefixLength $IPSubnetPrefix `
+-DefaultGateway $IPGateway
