ad75a7890029cc64cc2af90ceec2e5d3ca48f420
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2019-09-09 18:42:17 +0200

    Initial commit

--- Install-Google-Chrome.ps1
+++ Install-Google-Chrome.ps1
@@ -1,4 +1,6 @@
-$Installer = $env:TEMP + "\chrome_installer.exe"; 
+# Unattended Install of Google Chrome
+
+$Installer = $env:TEMP + "\chrome_installer.exe";
 Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Installer; 
 Start-Process -FilePath $Installer -Args "/silent /install" -Verb RunAs -Wait; 
 Remove-Item $Installer;
