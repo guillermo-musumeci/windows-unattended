82ed6131b74e0b95d63968f99345e249fec7520f
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2019-09-09 15:56:02 +0200

    Initial commit

--- Install-Google-Chrome.ps1
+++ Install-Google-Chrome.ps1
@@ -1,5 +1,4 @@
-$Path = $env:TEMP; 
-$Installer = "chrome_installer.exe"; 
-Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path\$Installer; 
-Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait; 
-Remove-Item $Path\$Installer
+$Installer = $env:TEMP + "\chrome_installer.exe"; 
+Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path; 
+Start-Process -FilePath $Installer -Args "/silent /install" -Verb RunAs -Wait; 
+Remove-Item $Installer;
